0267aa3ac1aa5f5645efcc1c0ab0e734282469ca
Toothbrush <adiraaj199@gmail.com>
2023-07-12 15:09:54 -0400

    Update clipmaker.sh

--- clipmaker.sh
+++ clipmaker.sh
@@ -2,9 +2,9 @@
 
 case "$1" in
 0) sleep 5 ;;
-1) ffmpeg -i output.mp4 -filter:v "crop=405:720:0:0" output2.mp4 ;;
-2) ffmpeg -i output.mp4 -filter:v "crop=405:720:320:0" output2.mp4 ;;
-3) ffmpeg -i output.mp4 -filter:v  "crop=405:720:500:0" output2.mp4 ;;
-4) ffmpeg -i output.mp4 -filter:v "crop=405:720:960:0" output2.mp4 ;;
-5) ffmpeg -i output.mp4 -filter:v  "crop=405:720:450:0" output2.mp4 ;;
+1) ffmpeg -i output.mp4 -filter:v "crop=405:720:0:0" output1.mp4 ;;
+2) ffmpeg -i output.mp4 -filter:v "crop=405:720:320:0" output1.mp4 ;;
+3) ffmpeg -i output.mp4 -filter:v  "crop=405:720:500:0" output1.mp4 ;;
+4) ffmpeg -i output.mp4 -filter:v "crop=405:720:960:0" output1.mp4 ;;
+5) ffmpeg -i output.mp4 -filter:v  "crop=405:720:450:0" output1.mp4 ;;
 esac
